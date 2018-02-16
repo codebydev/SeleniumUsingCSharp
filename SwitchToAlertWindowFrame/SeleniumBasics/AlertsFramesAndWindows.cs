058286f53c5619bc03803cb03e5eaab144b0ae4b
CodeByDev <codebydev@gmail.com>
2018-02-16 17:13:58 +0530

    SwitchToAlertWindowFrame

---
+++ SwitchToAlertWindowFrame/SeleniumBasics/AlertsFramesAndWindows.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SeleniumBasics
+{
+    [TestClass]
+    public class AlertsFramesAndWindows
+    {
+        [TestMethod]
+        public void SwitchToAlert()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Url = "file:///C:/Users/hanumanthargs/Downloads/HTML/test1.html";
+            driver.FindElement(By.Id("alert")).Click();
+
+            Thread.Sleep(2000);
+            string text = driver.SwitchTo().Alert().Text;
+            Console.WriteLine(text);
+            driver.SwitchTo().Alert().Accept(); // To click on ok button
+
+            Thread.Sleep(2000);
+            driver.Quit();
+        }
+
+        [TestMethod]
+        public void SwitchToConfirm()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Url = "file:///C:/Users/hanumanthargs/Downloads/HTML/test1.html";
+            driver.FindElement(By.Id("confirm")).Click();
+
+            Thread.Sleep(2000);
+            string text = driver.SwitchTo().Alert().Text;
+            Console.WriteLine(text);
+            driver.SwitchTo().Alert().Dismiss(); // To click on cancel button
+
+            Thread.Sleep(2000);
+            driver.Quit();
+        }
+
+        [TestMethod]
+        public void SwitchToPrompt()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Url = "file:///C:/Users/hanumanthargs/Downloads/HTML/test1.html";
+            driver.FindElement(By.Id("prompt")).Click();
+
+            Thread.Sleep(2000);
+            driver.SwitchTo().Alert().SendKeys("I am learning selenium using c#");
+            Thread.Sleep(2000);
+           
+            driver.SwitchTo().Alert().Accept(); // To click on cancel button
+
+            Thread.Sleep(2000);
+            driver.Quit();
+        }
+
+        [TestMethod]
+        public void SwitchToFrame()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Url = "https://netbanking.hdfcbank.com/netbanking/";
+
+            driver.SwitchTo().Frame("login_page");
+            driver.FindElement(By.Name("fldLoginUserId")).SendKeys("1234");
+
+            Thread.Sleep(4000);
+            driver.Quit();
+        }
+
+        [TestMethod]
+        public void SwitchToWindow()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Url = "https://netbanking.hdfcbank.com/netbanking/";
+
+            driver.SwitchTo().Frame("login_page");
+            Console.WriteLine("Before click");
+            Console.WriteLine("Number of windows "+driver.WindowHandles.Count);
+            foreach (var item in driver.WindowHandles)
+            {
+                Console.WriteLine(item);
+            }
+
+            driver.FindElement(By.LinkText("Click here")).Click();
+
+            Thread.Sleep(2000);
+            Console.WriteLine("After click");
+            Console.WriteLine("Number of windows " + driver.WindowHandles.Count);
+            foreach (var item in driver.WindowHandles)
+            {
+                Console.WriteLine(item);
+            }
+
+            driver.SwitchTo().Window(driver.WindowHandles[1]);
+
+            driver.FindElement(By.Name("fldLoginUserId")).SendKeys("1234");
+            
+            Thread.Sleep(4000);
+            driver.Close();
+
+            Console.WriteLine("After Closing the window");
+            Console.WriteLine("Number of windows " + driver.WindowHandles.Count);
+            foreach (var item in driver.WindowHandles)
+            {
+                Console.WriteLine(item);
+            }
+            driver.Quit();
+        }
+    }
+}
---
+++ SwitchToAlertWindowFrame/SeleniumBasics/SelBasics.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System.Threading;
+
+namespace SeleniumBasics
+{
+    [TestClass]
+    public class SelBasics
+    {
+        [TestMethod]
+        public void LaunchBrowser()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Url = "http://cricinfo.com";
+            driver.Quit();
+        }
+
+        [TestMethod]
+        public void ReadTitle()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Url = "file:///C:/Users/hanumanthargs/Desktop/Demo.html";
+            Console.WriteLine(driver.Title);
+            driver.Quit();
+        }
+
+        [TestMethod]
+        public void LocateByName()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Url = "https://www.theonlinecalculator.com/";
+            driver.FindElement(By.Name("four")).Click();
+            driver.FindElement(By.Name("add")).Click();
+            driver.FindElement(By.Name("two")).Click();
+            driver.FindElement(By.Name("calculate")).Click();
+            Thread.Sleep(2000);
+            driver.Quit();
+        }
+
+        [TestMethod]
+        public void LocateById()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Url = "https://www.facebook.com/";
+            driver.FindElement(By.Id("u_0_l")).SendKeys("Rahul");
+            Thread.Sleep(2000);
+            driver.Quit();
+        }
+
+        [TestMethod]
+        public void LocateByClassName()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Url = "file:///C:/Users/hanumanthargs/Desktop/Demo.html";
+            string text=driver.FindElement(By.ClassName("two")).Text;
+            Console.WriteLine(text);
+            Thread.Sleep(2000);
+            driver.Quit();
+        }
+
+        [TestMethod]
+        public void LocateByTagName()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Url = "file:///C:/Users/hanumanthargs/Desktop/Demo.html";
+            string text = driver.FindElement(By.TagName("p")).Text;
+            Console.WriteLine(text);
+            Thread.Sleep(2000);
+            driver.Quit();
+        }
+
+        [TestMethod]
+        public void LocateByLinkText()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Url = "file:///C:/Users/hanumanthargs/Desktop/Demo.html";
+            driver.FindElement(By.LinkText("Go to google")).Click();
+            Thread.Sleep(2000);
+            driver.Quit();
+        }
+
+        [TestMethod]
+        public void LocateByPartialLinkText()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Url = "file:///C:/Users/hanumanthargs/Desktop/Demo.html";
+            driver.FindElement(By.PartialLinkText("Go to")).Click();
+            Thread.Sleep(2000);
+            driver.Quit();
+        }
+
+
+    }
+}
