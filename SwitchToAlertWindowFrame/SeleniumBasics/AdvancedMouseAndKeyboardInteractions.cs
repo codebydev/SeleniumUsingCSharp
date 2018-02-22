aacfe1c6d291af2611177840e915943d186d5dcb
CodeByDev <codebydev@gmail.com>
2018-02-22 15:45:18 +0530

    Uploaded on 22 feb

---
+++ SwitchToAlertWindowFrame/SeleniumBasics/AdvancedMouseAndKeyboardInteractions.cs
@@ -0,0 +1,154 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Interactions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SeleniumBasics
+{
+    [TestClass]
+    public class AdvancedMouseAndKeyboardInteractions
+    {
+        [TestMethod]
+        public void DoublClick()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Url = "file:///C:/Users/hanumanthargs/Downloads/HTML/DoubleClick.html";
+
+            Thread.Sleep(4000);
+
+            Actions actions = new Actions(driver);
+
+            actions
+                .DoubleClick(driver.FindElement(By.Name("dblClick")))
+                .Build()
+                .Perform();
+
+            Thread.Sleep(4000);
+            
+
+            driver.Quit();          
+
+        }
+
+        [TestMethod]
+        public void DragAndDrop()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Url = "file:///C:/Users/hanumanthargs/Downloads/HTML/DragAndDrop.html";
+
+            Thread.Sleep(4000);
+
+            Actions actions = new Actions(driver);
+
+            actions
+                .DragAndDrop(driver.FindElement(By.Id("draggable")),driver.FindElement(By.Id("droppable")))
+                .Build()
+                .Perform();
+
+            Thread.Sleep(4000);
+
+            driver.Quit();
+        }
+
+        [TestMethod]
+        public void KeyDownAndKeyUp()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Url = "file:///C:/Users/hanumanthargs/Downloads/HTML/Selectable.html";
+
+            Thread.Sleep(4000);
+
+            Actions actions = new Actions(driver);
+
+            actions
+                .KeyDown(Keys.LeftControl)
+                .Click(driver.FindElement(By.Name("one")))
+                .Click(driver.FindElement(By.Name("six")))
+                .Click(driver.FindElement(By.Name("three")))
+                .Click(driver.FindElement(By.Name("ten")))
+                .Click(driver.FindElement(By.Name("five")))
+                .KeyUp(Keys.LeftControl)
+                .Build()
+                .Perform();
+
+            Thread.Sleep(4000);
+
+            driver.Quit();
+        }
+
+        [TestMethod]
+        public void RightClickOrContextClick()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Url = "file:///C:/Users/hanumanthargs/Downloads/HTML/Selectable.html";
+
+            Thread.Sleep(4000);
+
+            Actions actions = new Actions(driver);
+
+            actions
+                .MoveToElement(driver.FindElement(By.Name("four")))
+                .ContextClick()
+                .Build()
+                .Perform();
+
+            Thread.Sleep(4000);
+
+            driver.Quit();
+        }
+
+        [TestMethod]
+        public void ClickHoldAndRelease()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Url = "file:///C:/Users/hanumanthargs/Downloads/HTML/Selectable.html";
+
+            Thread.Sleep(4000);
+
+            Actions actions = new Actions(driver);
+
+            actions
+                .ClickAndHold(driver.FindElement(By.Name("one")))
+                .MoveToElement(driver.FindElement(By.Name("eleven")))
+                .Release()                
+                .Build()
+                .Perform();
+
+            Thread.Sleep(4000);
+
+            driver.Quit();
+        }
+
+        [TestMethod]
+        public void MoveByOffset()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Url = "file:///C:/Users/hanumanthargs/Downloads/HTML/Selectable.html";
+
+            Thread.Sleep(4000);
+
+            Actions actions = new Actions(driver);
+
+            Point p = driver.FindElement(By.Name("two")).Location;
+
+            actions
+                .MoveToElement(driver.FindElement(By.Name("eleven")))
+                .Click()
+                .MoveByOffset(20,20)
+                .ContextClick()                
+                .Build()
+                .Perform();
+
+            Thread.Sleep(4000);
+
+            driver.Quit();
+        }
+    }
+}
---
+++ SwitchToAlertWindowFrame/SeleniumBasics/DropDownCheckboxAndRadiobutton.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SeleniumBasics
+{
+    [TestClass]
+    public class DropDownCheckboxAndRadiobutton
+    {
+        [TestMethod]
+        public void RadioButtonTest()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Url = "https://www.facebook.com/";
+            Thread.Sleep(4000);
+
+            driver.FindElement(By.Id("u_0_c")).Click();
+            bool selected=driver.FindElement(By.Id("u_0_c")).Selected;
+            Console.WriteLine("Male radio button selected : "+selected);
+
+            selected = driver.FindElement(By.Id("u_0_b")).Selected;
+            Console.WriteLine("Female radio button selected : " + selected);
+
+            Thread.Sleep(4000);
+            driver.Quit();
+        }
+
+        [TestMethod]
+        public void CheckBoxTest()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Url = "http://uitestpractice.com/Students/Form";
+            Thread.Sleep(4000);
+
+            driver.FindElement(By.XPath("//input[@value='dance']")).Click();
+            bool selected = driver.FindElement(By.XPath("//input[@value='dance']")).Selected;
+            Console.WriteLine("Dancing check box selected : " + selected);
+
+            selected = driver.FindElement(By.XPath("//input[@value='read']")).Selected;
+            Console.WriteLine("Reading check box selected : " + selected);
+
+            Thread.Sleep(4000);
+            driver.Quit();
+        }
+
+        [TestMethod]
+        public void DropDownValueSelectTest()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Url = "https://www.facebook.com/";
+            Thread.Sleep(4000);
+
+            SelectElement dropdown = new SelectElement(driver.FindElement(By.Id("day")));
+            dropdown.SelectByText("4");
+
+            dropdown = new SelectElement(driver.FindElement(By.Id("month")));
+            dropdown.SelectByValue("12");
+
+            Thread.Sleep(4000);
+            driver.Quit();
+        }
+
+        [TestMethod]
+        public void CaptureScreenShot()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Url = "https://www.facebook.com/";
+            Thread.Sleep(4000);
+
+            ((ITakesScreenshot)driver).GetScreenshot().SaveAsFile("test.jpeg", ImageFormat.Jpeg);
+
+            driver.Quit();
+        }
+
+        [TestMethod]
+        public void ExecuteJavaScript()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Url = "https://www.facebook.com/";
+            Thread.Sleep(4000);
+
+            ((IJavaScriptExecutor)driver).ExecuteScript()
+
+            driver.Quit();
+        }
+
+    }
+    }
