e271a4c9238b5368008c453e4a3357e78f4c4d02
CodeByDev <codebydev@gmail.com>
2018-02-27 16:55:04 +0530

    Uploaded on 27th Feb

---
+++ SwitchToAlertWindowFrame/SeleniumBasics/FileUploadDemo.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SeleniumBasics
+{
+    [TestClass]
+    public class FileUploadDemo
+    {
+        [TestMethod]
+        public void FileUploadUsingAutoItTest()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Url = "http://uitestpractice.com/Students/Widgets";
+
+            Thread.Sleep(2000);
+            driver.FindElement(By.Id("image_file")).Click();
+            Process.Start("uploadImage.exe");
+
+            Thread.Sleep(4000);
+            driver.Quit();
+        }
+
+        [TestMethod]
+        public void FileUploadWithoutAutoItTest()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Url = "http://uitestpractice.com/Students/Widgets";
+            Thread.Sleep(2000);
+            
+            driver.FindElement(By.Id("image_file")).SendKeys("C:\\Users\\hanumanthargs\\Desktop\\VS 2017 installation.png");
+
+            Thread.Sleep(4000);
+            driver.Quit();
+        }
+    }
+}
---
+++ SwitchToAlertWindowFrame/SeleniumBasics/Synchronization.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SeleniumBasics
+{
+    [TestClass]
+    public class Synchronization
+    {
+        [TestMethod]
+        public void WaitUsingSleep()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Url = "http://uitestpractice.com/Students/Contact";
+
+            driver.FindElement(By.PartialLinkText("This is")).Click();
+
+            Thread.Sleep(15000);
+
+            string text = driver.FindElement(By.ClassName("ContactUs")).Text;
+            Assert.IsTrue(text.Contains("Python"));
+
+            driver.Quit();
+        }
+
+        [TestMethod]
+        public void WaitUsingImplicitWait()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(15));
+            driver.Url = "http://uitestpractice.com/Students/Contact";
+
+            driver.FindElement(By.PartialLinkText("This is")).Click();
+
+            // Thread.Sleep(15000);
+
+            string text = driver.FindElement(By.ClassName("ContactUs")).Text;
+            Assert.IsTrue(text.Contains("Python"));
+
+            driver.Quit();
+        }
+
+        [TestMethod]
+        public void WaitUsingExplicitWait()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            // driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(15));
+            driver.Url = "http://uitestpractice.com/Students/Contact";
+
+            driver.FindElement(By.PartialLinkText("This is")).Click();
+
+            // Thread.Sleep(15000);
+
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(15));
+            wait.PollingInterval = TimeSpan.FromMilliseconds(50);
+
+            wait.Until(ExpectedConditions.ElementIsVisible(By.ClassName("ContactUs")));
+
+            string text = driver.FindElement(By.ClassName("ContactUs")).Text;
+            Assert.IsTrue(text.Contains("Python"));
+
+            driver.Quit();
+        }
+
+        [TestMethod]
+        public void WaitUsingPageLoadTimeOut()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            // driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(15));
+            driver.Manage().Timeouts().SetPageLoadTimeout(TimeSpan.FromSeconds(150));
+            driver.Url = "http://uitestpractice.com/Students/Contact";
+
+            driver.FindElement(By.PartialLinkText("This is")).Click();
+
+            // Thread.Sleep(15000);
+
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(15));
+
+            wait.Until(ExpectedConditions.ElementIsVisible(By.ClassName("ContactUs")));
+
+            string text = driver.FindElement(By.ClassName("ContactUs")).Text;
+            Assert.IsTrue(text.Contains("Python"));
+
+            driver.Quit();
+        }
+
+        [TestMethod]
+        public void WaitUsingScriptTimeOut()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            // driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(15));
+            driver.Manage().Timeouts().SetScriptTimeout(TimeSpan.FromSeconds(150));
+            driver.Url = "http://uitestpractice.com/Students/Contact";
+
+            driver.FindElement(By.PartialLinkText("This is")).Click();
+
+            // Thread.Sleep(15000);
+
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(15));
+
+            wait.Until(ExpectedConditions.ElementIsVisible(By.ClassName("ContactUs")));
+
+            string text = driver.FindElement(By.ClassName("ContactUs")).Text;
+            Assert.IsTrue(text.Contains("Python"));
+
+            driver.Quit();
+        }
+
+
+    }
+}
