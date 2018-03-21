15145e4b6c5191010349ee21f07bee32a748caa4
CodeByDev <codebydev@gmail.com>
2018-03-21 22:17:48 +0530

    Selenium grid demo

---
+++ SeleniumGrid/SeleniumGrid/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Remote;
+
+namespace SeleniumGrid
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // IWebDriver driver = new FirefoxDriver();
+
+            DesiredCapabilities capabilities = DesiredCapabilities.Chrome();
+            IWebDriver driver = new RemoteWebDriver(new Uri("http://10.98.232.220:4444/wd/hub"),capabilities);
+
+            driver.Url = "http://google.com";
+
+            driver.Quit();
+        }
+    }
+}
